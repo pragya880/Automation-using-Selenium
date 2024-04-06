e39ed072cc324b8818bf6ae7c0b498d80b73de88
pragya880 <63944374+pragya880@users.noreply.github.com>
2024-04-06 11:01:11 +0530

    Update main4.py

--- selenium/main4.py
+++ selenium/main4.py
@@ -13,7 +13,7 @@ options.add_experimental_option("detach", True)
 s = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=s, options=options)
 
-driver.get("https://cpidev.preoss.in/")
+driver.get("NAME OF THE WEBSITE")
 
 # Use WebDriverWait to wait for the element to be interactable
 wait = WebDriverWait(driver, 10)
@@ -30,28 +30,28 @@ def retry_action(action, max_attempts=5, delay=2):
     raise Exception("Failed after multiple attempts.")
 
 # Find and interact with the EmployeeID field
-retry_action(lambda: wait.until(EC.element_to_be_clickable((By.NAME, "EmployeeID")))).send_keys("103422")
+retry_action(lambda: wait.until(EC.element_to_be_clickable((By.NAME, "EmployeeID")))).send_keys("ID")
 
 # Find and click the send OTP button
 retry_action(lambda: wait.until(EC.element_to_be_clickable((By.ID, "send-otp")))).click()
 
 # Assuming there might be a delay in the OTP field becoming available
-retry_action(lambda: wait.until(EC.element_to_be_clickable((By.NAME, "otp")))).send_keys("101023")
+retry_action(lambda: wait.until(EC.element_to_be_clickable((By.NAME, "otp")))).send_keys("PASSWORD")
 
 # Find and click the verify OTP button
 retry_action(lambda: wait.until(EC.element_to_be_clickable((By.ID, "send-otp-verify")))).click()
 
 # Wait for the page title to match the expected title indicating a successful login
-retry_action(lambda: wait.until(EC.title_is("Dashboard | PREOSS")))
+retry_action(lambda: wait.until(EC.title_is("HOPEPAGE")))
 
 # After successful login, navigate to the new URL
-driver.get("https://cpidev.preoss.in/preoss-leads")
+driver.get("https://HYPERLINK")
 
 # Re-locate the "Add" button after the page transition
 retry_action(lambda: wait.until(EC.element_to_be_clickable((By.ID, "add-lead-sidebar-btn")))).click()
 
 # Save a screenshot to a specific path (Ensure the directory exists or adjust the path accordingly)
-driver.save_screenshot("D:\\insomniac\\screenshot\\homepage2.png")
+driver.save_screenshot("NAME.png")
 
 # Consider adding error handling or more checks here as needed
 
